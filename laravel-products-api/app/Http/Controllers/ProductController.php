aa21c98f90eeadbad510f5efe69acb833da15d6e
Md Aman Ullah <amanu092@gmail.com>
2025-05-16 11:33:39 +0800

    first commit

---
+++ laravel-products-api/app/Http/Controllers/ProductController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $query = Product::query();
+        
+        // Search filter
+        if ($request->has('search')) {
+            $query->where('title', 'like', '%'.$request->search.'%');
+        }
+        
+        // Pagination
+        $perPage = $request->input('per_page', 10);
+        return $query->paginate($perPage);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric|min:0',
+            'thumbnail' => 'nullable|url',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation errors',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $product = Product::create($request->all());
+
+        return response()->json($product, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return $product;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'sometimes|required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'sometimes|required|numeric|min:0',
+            'thumbnail' => 'nullable|url',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validation errors',
+                'errors' => $validator->errors()
+            ], 422);
+        }
+
+        $product->update($request->all());
+
+        return response()->json($product);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response()->json(null, 204);
+    }
+}
---
+++ laravel-products-api/routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('/api/products', ProductController::class .'@index');
+// returns the form for adding a product
+Route::get('/api/products/create', ProductController::class . '@create');
+// adds a product to the database
+Route::post('/api/products', ProductController::class .'@store');
+// returns a page that shows a full post
+Route::get('/api/products/{product}', ProductController::class .'@show');
+// returns the form for editing a product
+Route::get('/api/products/{product}/edit', ProductController::class .'@edit');
+// updates a product
+Route::put('/api/products/{product}', ProductController::class .'@update');
+// deletes a product
+Route::delete('/api/products/{product}', ProductController::class .'@destroy');
